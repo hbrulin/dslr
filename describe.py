78b1ee32d1581cc61394efc15ea35a690ab124d1
babaoriley <helenebrulin@live.fr>
2021-02-22 02:59:19 +0100

    start but printing is shit

--- describe.py
+++ describe.py
@@ -1,36 +1,22 @@
 import pandas as pd
 import sys
-
-def get_data(filename) :
-    df = pd.read_csv(filename)
-    try:
-        df = pd.read_csv(filename)
-    except :
-        sys.exit("Error: File does not exist or has wrong format")
-
-    ari = df.iloc[0:len(df),6]
-    astr = df.iloc[0:len(df),7]
-    herb = df.iloc[0:len(df),8]
-    dada = df.iloc[0:len(df),9]
-    div = df.iloc[0:len(df),10]
-    mug = df.iloc[0:len(df),11]
-    runes = df.iloc[0:len(df),12]
-    hist = df.iloc[0:len(df),13]
-    trans = df.iloc[0:len(df),14]
-    pot = df.iloc[0:len(df),15]
-    comc = df.iloc[0:len(df),16]
-    charms = df.iloc[0:len(df),17]
-    fly = df.iloc[0:len(df),18]
-    print(fly)
+from describer import DataDescriber
     
-    
-    #m = len(ari)
-    
+def print_table(data):
+    rows = ["---", "Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"]
+    for row in rows:
+        print(f'{row:>10}')
+    for feature in data.columns:
+        if data.is_numeric(feature) :
+            print(f'{data.get_acronym(feature):>10}')
+            print(f'{data.count(feature):>10}')
+            print(f'{data.mean(feature):>10.6f}')
 
 def main():
     if (len(sys.argv) == 1) :
         sys.exit("Error: No input dataset")
-    get_data(sys.argv[1])
+    data = DataDescriber.get_data(sys.argv[1])
+    print_table(data)
 
 
 if __name__ == "__main__":
---
+++ describer.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import sys
+
+class DataDescriber(pd.DataFrame):
+    def get_data(filename: str):
+        try:
+            data = pd.read_csv(filename)
+        except :
+            sys.exit("Error: File does not exist or has wrong format")
+        return DataDescriber(data)
+
+    def get_acronym(self, name: str):
+        spl = name.split(" ")
+        if (len(spl) <= 2):
+            return name
+        acronym = ""
+        for word in spl :
+            acronym += word[0].upper()
+        return acronym
+
+    def is_numeric(self, feature: str):
+        return np.issubdtype(self[feature].dtype, np.number)
+
+    #dropna removes missing value
+    def count(self, feature: str):
+        return len(self[feature].dropna())
+
+    def mean(self, feature: str) -> float:
+        return sum(self[feature].dropna()) / self.count(feature)
+    
+    #def std
