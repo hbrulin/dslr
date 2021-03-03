f09fe17d57a6eb56709634bde974a645cafa3723
babaoriley <helenebrulin@live.fr>
2021-03-04 00:02:33 +0100

    no opti

--- logreg_train.py
+++ logreg_train.py
@@ -7,20 +7,6 @@ from sorting_hat.multi_classifier import MultiClassifier
 from histogram import get_houses_total_diff
 from sorting_hat.predictor import Predictor
 
-"""
-#if issue with accuracy, don't use this because of arithmancy
-def top_least_homogenous(data, courses):
-     #answer = input('\33[32m' + "Y to train with non homogenous courses : " + '\33[0m')
-    #if answer == "Y":
-    #   courses = top_least_homogenous(data, courses) #not mandatory + if done i need to remove irrelevant y values : del data['Care of Magical Creatures']
-    diff_arr = []
-    for i, course in enumerate(courses):
-        houses_diff = get_houses_total_diff(data, course)
-        diff_arr.append([course, houses_diff])
-    least_homo = sorted(diff_arr[:6]) #get last six elements
-    #print(least_homo)
-"""
-
 def write_thetas(thetas):
     print('\33[32m' + "Calculated thetas are: " + '\33[0m')
     for theta in thetas:
@@ -30,16 +16,15 @@ def write_thetas(thetas):
         for theta in thetas:
             writer.writerow(theta)
 
-
 def main():
     ###deal with args
     if (len(sys.argv) == 1) :
         print('\33[31m' +"Error: No input file." + '\33[0m')
         sys.exit()
-    
+
     ##get necessary data
     data = DataDescriber.get_data(sys.argv[1])
-    courses = Utils.get_courses(data) #use least homogenous
+    courses = Utils.get_courses(data)
     X, Y = Utils.normalize(data, courses, Action.TRAINING)
     m = len(X)
 
